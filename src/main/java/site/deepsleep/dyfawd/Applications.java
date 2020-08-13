37e28cd06ff3908035b5e6779ed18ea3223c2d01
Sangwoon Park <nbiosupr@gmail.com>
2020-08-13 17:01:17 +0900

    JpaAuditing 기능 추가

--- src/main/java/site/deepsleep/dyfawd/Applications.java
+++ src/main/java/site/deepsleep/dyfawd/Applications.java
@@ -2,7 +2,9 @@ package site.deepsleep.dyfawd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Applications {
     public static void main(String[] args)  {
--- src/main/java/site/deepsleep/dyfawd/domain/collecteddata/SensorData.java
+++ src/main/java/site/deepsleep/dyfawd/domain/collecteddata/SensorData.java
@@ -3,13 +3,14 @@ package site.deepsleep.dyfawd.domain.collecteddata;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import site.deepsleep.dyfawd.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class SensorData {
+public class SensorData extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
